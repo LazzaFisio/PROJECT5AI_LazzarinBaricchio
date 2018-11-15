ffbc5d07b4d4622d6d65ad0cab20c6dcadfc3fa5
Leonardo <leonardolazzarin14@gmail.com>
2018-11-15 08:56:42 +0100

    Creazione outlet ed action

--- Progetto/Progetto/ViewController.swift
+++ Progetto/Progetto/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lato: UIView!
+    @IBOutlet weak var risLabel: UILabel!
+    @IBOutlet weak var ris: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func calcola(_ sender: Any) {
+    }
+    
 }
 
