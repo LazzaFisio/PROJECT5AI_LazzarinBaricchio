16c593bb298e7e366c7aa1b6633973b75f61a6a6
Leonardo <leonardolazzarin14@gmail.com>
2018-11-15 09:24:14 +0100

    Progetto finito

--- Progetto/Progetto/ViewController.swift
+++ Progetto/Progetto/ViewController.swift
@@ -10,27 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var lato: UIView!
     @IBOutlet weak var risLabel: UILabel!
     @IBOutlet weak var ris: UILabel!
   
+    @IBOutlet weak var lato: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
   
     @IBAction func calcola(_ sender: Any) {
+        ris.isHidden = false
+        risLabel.isHidden = false
+        ris.text = String(calcoloArea(intero: fromSringToInt(testo: lato.text!)))
     }
   
     func fromSringToInt(testo : String) -> Int
     {
-        var intero = Int(testo)
+        let intero = Int(testo)
         return intero!
     }
 
     func calcoloArea(intero : Int) -> Int
     {
-        var area = intero^2
+        let area = intero^2
         return area
     }
 }
